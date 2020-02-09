dcd8c52d2e5b225983184d52dd079b0c73eb2903
Siddharth Isaiah <siddharthisaiah@gmail.com>
2020-02-09 21:16:34 +0530

    More views and viewsets

--- polls/urls.py
+++ polls/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from .views import polls_list, polls_detail
-from .apiviews import PollList, PollDetail, ChoiceList, CreateVote
+from .apiviews import ChoiceList, CreateVote #, PollList, PollDetail
+from .apiviews import PollViewSet
+
+router = DefaultRouter()
+router.register('polls', PollViewSet, basename='polls')
 
 urlpatterns = [
-    path('polls/', PollList.as_view(), name='polls_list'),
-    path('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
-    path('choices/', ChoiceList.as_view(), name='polls_choices'),
-    path('vote/', CreateVote.as_view(), name='create_vote')
+    # path('polls/', PollViewSet, name='polls_list'),
+    # path('polls/<int:pk>/', PollViewSet, name='polls_detail'),
+    path('polls/<int:pk>/choices/', ChoiceList.as_view(), name='polls_choices'),
+    path('polls/<int:pk>/choices/<int:choice_pk>/vote/',
+         CreateVote.as_view(), name='create_vote')
 ]
+
+urlpatterns += router.urls
